beebfdd5a4afd485de553ee456d2769b34d05bde
Dennis Witt <94747795+wittdennis@users.noreply.github.com>
2025-04-04 12:27:25 +0200

    chore: update for macos use

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-ansible-galaxy install -r requirements.yml
-ansible-playbook -i inventory main.yml -K $@
+/opt/homebrew/bin/ansible-galaxy install -r requirements.yml
+/opt/homebrew/bin/ansible-playbook -i inventory main.yml -K $@
