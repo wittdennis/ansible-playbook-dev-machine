99081962eded37d38fad9ee653f6cd29ffab1e3d
Dennis Witt <94747795+wittdennis@users.noreply.github.com>
2025-04-04 13:57:16 +0200

    chore: set brew base path for run script

--- run.sh
+++ run.sh
@@ -1,4 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
-/opt/homebrew/bin/ansible-galaxy install -r requirements.yml
-/opt/homebrew/bin/ansible-playbook -i inventory main.yml -K $@
+PLATFORM="undefined"
+case "$OSTYPE" in
+solaris*) PLATFORM=solaris ;;
+darwin*) PLATFORM=osx ;;
+linux*) PLATFORM=linux ;;
+bsd*) PLATFORM=bsd ;;
+msys*) PLATFORM=windows ;;
+cygwin*) PLATFORM=windows ;;
+*) PLATFORM=undefined ;;
+esac
+
+BASEPATH=/usr/local/bin
+if [ $PLATFORM = 'osx' ]; then
+    BASEPATH=/opt/homebrew/bin
+elif [ $PLATFORM = 'linux' ]; then
+    BASEPATH=/home/linuxbrew/.linuxbrew/bin
+else
+    BASEPATH=/usr/local/bin
+fi
+
+${BASEPATH}/ansible-galaxy install -r requirements.yml
+${BASEPATH}/ansible-playbook -i inventory main.yml -K $@
